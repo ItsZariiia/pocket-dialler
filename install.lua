e55903d16e4b1ffabe5dd57ebe7605b827095327
Zari <itszariiia@gmail.com>
2026-05-22 08:17:04 +0100

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,4 @@
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/client")
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/common")
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/server")
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/addresses.conf")
