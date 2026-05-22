869f6f0d383c243335a5e76c5e70d6461a9c8d49
Zari <itszariiia@gmail.com>
2026-05-22 08:20:44 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,4 +1,5 @@
-shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/client")
-shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/common")
-shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/server")
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/client/*")
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/common/*")
+shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/server/*")
 shell.run("wget https://github.com/ItsZariiia/pocket-dialler/tree/dev/addresses.conf")
+
